c58134cceba1987046c687ee8265a9084e395053
Alec Mirambeau <alecmirambeau@gmail.com>
2026-04-16 19:03:52 -0400

    where at before unable to run code

--- othello-game-server/sdks/python/client.py
+++ othello-game-server/sdks/python/client.py
@@ -6,19 +6,27 @@ import socket
 
 current_board = None
 player = None
+opposing_player = None
+
 def get_move(playerNum, board):
     # this will assign a reference to the passed board but at a global level.
     current_board = board
-   #  update player to be the player value. it doesn't matter if we do this everytime since the player won't change.
+   #  update player to be the player value. it doesn't matter if we do this every time since the player won't change.
     player = playerNum
+
+    #  update opposing_player to be the player value. it doesn't matter if we do this every time since the player won't change.
+    opposing_player = 2 if player == 1 else 1
+    print("here")
+
    # TODO determine valid moves
     move_to_make = make_move()
     # TODO determine best move
-    return move_to_make
+
+    return [2, 3]
 
 def make_move():
     # determine all of the valid moves
-    all_valid_moves = determine_valid_moves(player, board)
+    all_valid_moves = determine_valid_moves()
     # Once we have all valid moves, determine the best move to make
     move_to_make = determine_best_move()
     return move_to_make
@@ -28,24 +36,62 @@ def determine_valid_moves():
     :return: a 2D array where each sub-array contains two integers representing a valid move that this player can make,
     therefore returning all the valid moves this player can make.
     """
+    # utilize set for two properties:
+    # 1. No need to check for duplicate moves
+    # 2. Faster look up
+    # This will be a set of tuples where each tuple is a valid move
+    all_moves = set()
+    # we will get all potential moves and then need to filter out the real moves available
+    potential_moves = set()
     for i in range(0, 8):
         row = board[i]
         for j, val in enumerate(row):
-            if val != player:
-                is_valid_space(val, j, i)
+            # check to make sure the val in the space is not the current player and also is the
+            # opposing player to ensure we aren't checking spaces that aren't even touching the rest
+            # of the chips because that would be invalid. Have to sandwich opposing player chips.
+            if val == opposing_player:
+                possible_moves = moves_available_in_surrounding_spaces(val, i, j)
+                potential_moves.union(possible_moves)
+
+#     We have acquired all potential moves. Let's filter out the moves that don't have another one of our chips in
+# any direction, because we need to sandwich the opponent chips by having our chips on both ends. If we choose a move
+# that won't have one of our chips in any of the rows/columns/diagonals then it's an invalid move.
+
+    print("got to end")
 
 
-def is_valid_space(val, col, row):
+
+                    
+
+
+def moves_available_in_surrounding_spaces(val, row, col):
     """
     :param val: int representing if this space has been played already, and if so, by whom.
     :param col: int for column number representing the space we want to check if it's playable.
     :param row: into for row number representing the space we want to check if it's playable.
     :return: boolean representing if this space is playable.
     """
+    potential_moves = set()
     # first check if all the spaces around this piece are already filled as we don't want to waste time checking
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            curr_space = (row + i, col + j)
+            curr_row = curr_space[0]
+            curr_col = curr_space[1]
+            # make sure the column and rows are in bounds
+            if 0 <= curr_row < 8 and 0 <= curr_col < 8:
+                # make sure this spot is empty and therefor available
+                if board[curr_row][curr_col] == 0:
+                    # if this space is available, lets add it to the potential spaces
+                    potential_moves.add(curr_space)
+    return potential_moves
+
+
 #     utilize try catch block and double for loops to check all 8 squares around this specific space.
 # try catch block so that it'll automatically handle out of bounds exceptions with less code. Look into faster
 # ways to do this
+    
+    
 
 
 def determine_best_move():
